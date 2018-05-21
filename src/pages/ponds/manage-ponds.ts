229126266e0d4932fe71f795df27880f9dce8c04
jem <jem.m.t.donato@accenture.com>
2018-05-21 17:53:27 +0800

    added cards

--- src/pages/ponds/manage-ponds.ts
+++ src/pages/ponds/manage-ponds.ts
@@ -1,11 +1,43 @@
-import {Component} from "@angular/core";
+import { Component, Input, ViewChild, ElementRef, Renderer } from "@angular/core";
 import {NavController, NavParams} from 'ionic-angular';
-import { JELLY_PROPERTIES } from 'config/jelly.properties';
+import { JELLY_PROPERTIES } from '../../../config/jelly.properties';
+import { ExpandableComponent } from '../util/expandable-list/expandable';
+import { Modal, ModalController, ModalOptions } from 'ionic-angular';
 
 @Component({
   selector: 'page-manage-ponds',
   templateUrl: 'manage-ponds.html'
 })
 export class ManagePondsPage {
-  
+    constructor(private modal: ModalController) {
+
+  }
+
+  openModal() {
+
+    const myModalOptions: ModalOptions = {
+      enableBackdropDismiss: false
+    };
+
+    const myModalData = {
+      name: 'Paul Halliday',
+      occupation: 'Developer'
+    };
+
+    const myModal: Modal = this.modal.create('ModalPage', { data: myModalData }, myModalOptions);
+
+    myModal.present();
+
+    myModal.onDidDismiss((data) => {
+      console.log("I have dismissed.");
+      console.log(data);
+    });
+
+    myModal.onWillDismiss((data) => {
+      console.log("I'm about to dismiss");
+      console.log(data);
+    });
+
+  }
 }
+
