0f6d0083e3708a92e3f4d5884f8248434906bb7b
jem <jem.m.t.donato@accenture.com>
2018-05-24 18:09:55 +0800

    added card icons and other pages

---
+++ src/pages/history/history.ts
@@ -0,0 +1,13 @@
+import { Component, Input, ViewChild, ElementRef, Renderer } from "@angular/core";
+import {NavController, NavParams} from 'ionic-angular';
+import { JELLY_PROPERTIES } from '../../../config/jelly.properties';
+import { Modal, ModalController, ModalOptions } from 'ionic-angular';
+
+@Component({
+  selector: 'history',
+  templateUrl: 'history.html'
+})
+export class HistoryPage {
+
+}
+
